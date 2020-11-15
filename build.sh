81dd798887f09197026d4eea2634b437fd34ff29
Jonathan Merritt <j.s.merritt@gmail.com>
2020-11-15 19:03:16 +1100

    Setup toolchain wrappers

--- build.sh
+++ build.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 readonly dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 readonly root="$( cd "$dir" && pwd )"
 
+export TARGETS='x86_64-linux-android armv7-linux-androideabi aarch64-linux-android'
 export NDK_HOME="${root}/ndk"
 export NDK_VERSION='r21d'
+export ANDROID_HOME="$NDK_HOME"  # for the toolchain-wrapper scripts
 
 ./scripts/fetch-ndk.sh
+./scripts/bootstrap-wrappers.sh
---
+++ clean.sh
@@ -0,0 +1,17 @@
+#/usr/bin/env bash
+set -euo pipefail
+
+readonly dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+readonly root="$( cd "$dir" && pwd )"
+
+export NDK_HOME="${root}/ndk"
+
+echo "Cleaning"
+
+echo "Removing ndk directory"
+rm -rf "$NDK_HOME"
+
+echo "Resetting toolchain-wrapper"
+pushd toolchain-wrapper >/dev/null
+git reset --hard
+popd >/dev/null
